358738907edbb759c3df43ac94f7cb2e54e2251c
chrelyonly <1172576293@qq.com>
2025-09-23 09:24:10 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import oauth2 from "@/views/oauth2/oauth2.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/oauth2',
+      name: '开放授权',
+      component: oauth2
     },
   ]
 })
